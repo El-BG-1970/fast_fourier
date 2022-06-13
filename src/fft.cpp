103c22254eca864c2b0864f6197fe7de7caab98f
remyjck <remy.seassau@polytechnique.edu>
2022-06-13 15:24:34 +0200

    Fixed fft complex power bug

--- src/fft.cpp
+++ src/fft.cpp
@@ -8,7 +8,7 @@ std::complex<double> *radix2fft(double *P, size_t n) {
 		  return ret;
 	 }
 
-	 size_t half_n = (n/2) - 1;
+	 size_t half_n = n/2;
 	 
 	 std::complex<double> *ret;
 	 double *U, *V;
@@ -24,13 +24,14 @@ std::complex<double> *radix2fft(double *P, size_t n) {
 	 std::complex<double> *Ut = radix2fft(U, half_n);
 	 std::complex<double> *Vt = radix2fft(V, half_n);
 	 
-	 std::complex<double> om_n = std::exp((2/n)*M_PI*1i);
-	 std::complex<double> om = 1;
+	 std::complex<double> coeff;
+	 std::complex<double> omega;
 
 	 for (size_t j = 0; j < half_n; j++) {
-		  ret[j] = Ut[j] + om*Vt[j];
-		  ret[j+(n/2)] = Ut[j] - om*Vt[j];
-		  om = om * om_n;
+		coeff = std::complex<double>(0, M_PI*2*j/n);
+		omega = std::pow(M_E, coeff);
+		ret[j] = Ut[j] + omega*Vt[j];
+		ret[j+half_n] = Ut[j] - omega*Vt[j];
 	 }
 	 
 	 free(U);
@@ -54,20 +55,21 @@ double *inv_radix2fft(std::complex<double> *P, size_t n) {
 	 V = (std::complex<double>*) malloc(half_n*sizeof(std::complex<double>));
 
 	 for (size_t j = 0; j < half_n; j++) {
-		  U[j] = P[2*j];
-		  V[j] = P[2*j+1];
+		U[j] = P[2*j];
+		V[j] = P[2*j+1];
 	 }
 
 	 double *Ut = inv_radix2fft(U, half_n);
 	 double *Vt = inv_radix2fft(V, half_n);
 	 
-	 std::complex<double> om_n = std::exp(-(2/n)*M_PI*1i);
-	 std::complex<double> om = 1;
+	 std::complex<double> coeff;
+	 std::complex<double> omega;
 
 	 for (size_t j = 0; j < half_n; j++) {
-		  ret[j] = std::real(Ut[j] + om*Vt[j])/2;
-		  ret[j+(n/2)] = std::real(Ut[j] - om*Vt[j])/2;
-		  om = om * om_n;
+		coeff = std::complex<double>(0, M_PI*2*j/n);
+		omega = std::pow(M_E, coeff);
+		ret[j] = std::real(Ut[j] + omega*Vt[j])/2;
+		ret[j+(n/2)] = std::real(Ut[j] - omega*Vt[j])/2;
 	 }
 	 
 	 free(U);
@@ -84,8 +86,8 @@ double *fft_poly_mult(double *p1, size_t n1, double *p2, size_t n2) {
 	 double *Q = (double *)calloc(total, sizeof(double));
 	 std::complex<double> *tmp = (std::complex<double> *)calloc(total, sizeof(std::complex<double>));
 
-	 for (size_t i = 0; i < n1; P[i] = p1[i++]);
-	 for (size_t i = 0; i < n2; Q[i] = p2[i++]);
+	 for (size_t i = 0; i < n1; i++) {P[i] = p1[i];}
+	 for (size_t i = 0; i < n2; i++) {Q[i] = p2[i];}
 
 	 std::complex<double> *Pt = radix2fft(P, total);
 	 std::complex<double> *Qt = radix2fft(Q, total);
